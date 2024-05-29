bb762e15b950c962898d94742891c104a270ac6b
rogersrwr <160086701+rogersrwr@users.noreply.github.com>
2024-05-29 15:43:21 -0700

    smolo

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,9 +6,9 @@ const password = process.env.PASS;
 test('test', async ({ page }) => {
   await page.goto('https://voice.google.com/about');
   await page.getByRole('link', { name: 'Sign in' }).click();
-  await page.getByLabel('Email or phone').fill('brightarrowtest1@gmail.com');
+  await page.getByLabel('Email or phone').fill(`${{login}}`);
   await page.getByRole('button', { name: 'Next' }).click();
-  await page.getByLabel('Enter your password').fill('testingT6T13BA!');
+  await page.getByLabel('Enter your password').fill(`${password}`);
   await page.getByRole('button', { name: 'Next' }).click();
   await page.getByRole('tab', { name: 'Messages' }).click();
   //await page.getByLabel('Message by ‪79041‬: test msg').click();

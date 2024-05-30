25a76ed4dd1babdc8d5c5e9a966f2cdfa781ae17
rogersrwr <160086701+rogersrwr@users.noreply.github.com>
2024-05-29 19:43:42 -0700

    more test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,6 +2,9 @@ import { test, expect } from '@playwright/test';
 
 const login = process.env.LOGIN;
 const password = process.env.PASS;
+const tUser = process.env.TNOW_USER;
+const tPass = process.env.TNOW_PASS;
+
 
 test('test', async ({ page }) => {
   test.slow();
@@ -13,6 +16,56 @@ test('test', async ({ page }) => {
   await page.getByLabel('Enter your password').fill(`${password}`);
   //await page.getByRole('button', { name: 'Next' }).click();
   await page.getByLabel('Enter your password').press('Enter');
+
+  await expect(page.locator('15PPKe').filter({ hasText: 'Confirm your recovery phone' })).toBeVisible();
+  if (await page.locator('15PPKe').filter({ hasText: 'Confirm your recovery phone' }).isVisible()) {
+    await page.goto("google.com");
+  }
+
   await page.getByRole('tab', { name: 'Messages' }).click();
   //await page.getByLabel('Message by ‪79041‬: test msg').click();
 });
+
+
+
+/*
+test('test', async ({ page }) => {
+  await page.goto('https://www.textnow.com/');
+  await page.getByRole('link', { name: 'Web Messaging' }).click();
+  await page.getByLabel('Email or Username').click();
+  await page.getByLabel('Email or Username').fill('');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('');
+  await page.getByRole('button', { name: 'Log In' }).click();
+
+  if (await page.getByText('Verify you are a human').isVisible) {
+    await page.getByLabel('Press and Hold').click({delay: 10000 });
+  }
+
+  if (await page.getByText('Verify you are a human').isVisible) {
+    await page.getByLabel('Press and Hold').click({delay: 10000 });
+  }
+
+
+  //await page.getByLabel('Press and Hold').click({delay: 10000 });
+});
+
+test('test-gvoice', async ({ page }) => {
+  await page.getByRole('link', { name: 'Confirm your recovery phone' });
+
+});
+*/
+
+
+/*
+await expect(page.getByText('Verify it’s you', { exact: true })).toBeVisible();
+  await expect(page.getByText('Choose how you want to sign')).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Get a call at (•••) •••-••14' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Use another phone or computer' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Confirm your recovery phone' })).toBeVisible();
+  await page.getByRole('link', { name: 'Confirm your recovery phone' }).click();
+  await expect(page.getByText('Confirm the phone number you')).toBeVisible();
+  await page.getByLabel('Phone number').click();
+  await page.getByLabel('Phone number').fill('ohonenumnebre');
+  await page.getByRole('button', { name: 'Try another way' }).click();
+  */
